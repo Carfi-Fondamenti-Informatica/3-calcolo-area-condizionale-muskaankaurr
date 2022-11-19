a4ed4b18f8d113499998f35088c6c23db3848294
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-11-19 17:29:06 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 using namespace std;
 int main() {
-   float a=0, b=0;
+   float a=0,b=0;
    int opzione=0;
-   cin >> a >> b >> opzione;
+   cin >> a>> b >> opzione; 
    switch (opzione){
-       case 0 :
-          cout<<(a*b)/2<< endl;
-           break;
-       case 1 :
-          cout<<a*a<< endl;
-           break;
-       case 2 :
-           cout<<a*b<< endl;
-           break;
-   default:
-        cout<<"opzione non valida"<< endl;
-   }
+      case 0 :
+         cout<< (a*b)/2 << endl;
+         break;
+      case 1 : 
+         cout << (a*a) << endl;
+         break;
+      case 2:
+         cout << (a*b) <<endl;
+         break;
+      defaul:
+         cout<< "operazione non valida"<< endl;
+       }  
    return 0;
 }
