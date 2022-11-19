dd71ca14dd1187700658b01ee70edaefd529a108
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-11-19 17:49:33 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main() {
          cout << (a*b)/2 << endl;
          break;
       case 1 : 
-         cout << (a*a) << endl;
+         cout << a*a << endl;
          break;
       case 2:
-         cout << (a*b) << endl;
+         cout << a*b << endl;
          break;
       default:
          cout<< "opzione non valida"<< endl;
