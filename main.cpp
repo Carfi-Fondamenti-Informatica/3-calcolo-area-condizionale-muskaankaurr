604de4a5fea5c1c1b6c6858ffccca69ba4821c7b
muskaankaurr <115504844+muskaankaurr@users.noreply.github.com>
2022-11-19 17:47:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@ int main() {
    switch (opzione){
       case 0 :
          cout << (a*b)/2 << endl;
+         break;
       case 1 : 
          cout << (a*a) << endl;
+         break;
       case 2:
          cout << (a*b) << endl;
          break;
